7e8a9bd6f54fad180c28213a85ba3685584b6213
Matthew Kataryniak <mwk9@njit.edu>
2018-02-08 20:58:48 -0500

    Basis for player movement

---
+++ Assets/Scripts/LerpTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpTest : MonoBehaviour 
+{
+	[SerializeField]
+	private Color[] colors;
+	private SpriteRenderer sr;
+	[SerializeField]
+	private float duration;
+	private float t = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		sr.material.color = Color.Lerp(colors[0], colors[1], t);
+		if (t < 1.0f)
+		{
+			t += Time.deltaTime / duration;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	private Rigidbody2D rb;
+	[SerializeField]
+	private float speed;
+	private bool facingRight;
+	//[SerializeField]
+	//private Texture2D cursorTexture;
+	//public CursorMode cursorMode = CursorMode.Auto;
+	//[SerializeField]
+	//private Vector2 hotspot = Vector2.zero;
+	[SerializeField]
+	private GameObject cursorObj;
+	[SerializeField]
+	private float cursorDistance;
+
+	public bool controllerConnected;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+
+		facingRight = true;
+		//Cursor.SetCursor(cursorTexture, hotspot, cursorMode);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+		float hor = Input.GetAxis("Horizontal");
+		float ver = Input.GetAxis("Vertical");
+
+		rb.velocity = new Vector2(
+			Mathf.Lerp(0, hor * speed, 0.8f),
+			Mathf.Lerp(0, ver * speed, 0.8f)
+		);
+
+		if (cursorObj != null)
+		{
+			if (controllerConnected)
+			{
+				if (facingRight)
+					cursorObj.transform.localPosition = new Vector3(Input.GetAxis("Horizontal2") * cursorDistance, Input.GetAxis("Vertical2") * cursorDistance, 0);
+				else
+					cursorObj.transform.localPosition = new Vector3(Input.GetAxis("Horizontal2") * cursorDistance * -1, Input.GetAxis("Vertical2") * cursorDistance, 0);
+			}
+			else
+			{
+				cursorObj.transform.position = Vector2.Lerp(cursorObj.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 1.0f);
+				//cursorObj.transform.localPosition = Input.mousePosition;
+			}
+		}
+
+		ChangeDirection(hor);
+	}
+
+	public void ChangeDirection(float hor)
+	{
+		if (hor > 0 && !facingRight || hor < 0 && facingRight)
+		{
+			facingRight = !facingRight;
+			Vector3 scale = transform.localScale;
+			scale.x *= -1;
+			transform.localScale = scale;
+		}
+	}
+}
