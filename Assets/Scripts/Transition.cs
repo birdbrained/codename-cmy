3865333bfa63bdb577e0d0e0dcf03240dda13d04
Matthew Kataryniak <mwk9@njit.edu>
2018-02-28 09:26:30 -0500

    Added a cool transition effect shader

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Transition : MonoBehaviour 
+{
+	[SerializeField]
+	private Material transitionMaterial;
+
+	void OnRenderImage(RenderTexture src, RenderTexture dst)
+	{
+		Graphics.Blit(src, dst, transitionMaterial);
+	}
+}
