27479aada89530e3821f19030c910bc170cc8da2
Matthew Kataryniak <mwk9@njit.edu>
2018-05-07 03:13:07 -0400

    Started to work on a boss select screen

---
+++ Assets/Scripts/BossButtonManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BossButtonManager : MonoBehaviour
+{
+    private EventSystem es;
+    private BossSelectButton currButton;
+    [SerializeField]
+    private Text nameText;
+    [SerializeField]
+    private Text descText;
+    [SerializeField]
+    private Image image;
+
+	// Use this for initialization
+	void Start ()
+    {
+        es = EventSystem.current;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currButton = es.currentSelectedGameObject.GetComponent<BossSelectButton>();
+        if (currButton != null)
+        {
+            if (nameText != null)
+            {
+                nameText.text = currButton.name;
+            }
+            if (descText != null)
+            {
+                descText.text = currButton.desc;
+            }
+            if (image != null)
+            {
+                image.sprite = currButton.sprite;
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/BossSelectButton.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class BossSelectButton : MonoBehaviour
+{
+    public string name;
+    public string desc;
+    public Sprite sprite;
+}
